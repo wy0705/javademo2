9878707ed1de030b6b2eeeb4a6a931ca1351648e
wy0705 <2652918156@qq.com>
2021-11-21 20:50:02 +0800

    66

--- src/Solution4.java
+++ src/Solution4.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution4 {
     public int maxSubArray(int[] nums) {
         int pre = 0, maxAns = nums[0];
@@ -6,6 +9,21 @@ public class Solution4 {
             maxAns = Math.max(maxAns, pre);
         }
         return maxAns;
+
+    }
+}
+class Solution5 {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = dic.getOrDefault(s.charAt(j), -1); // 获取索引 i
+            dic.put(s.charAt(j), j); // 更新哈希表
+            tmp = tmp < j - i ? tmp + 1 : j - i; // dp[j - 1] -> dp[j]
+            res = Math.max(res, tmp); // max(dp[j - 1], dp[j])
+        }
+        return res;
+
 
     }
 }
