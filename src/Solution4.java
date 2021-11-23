9b4b8b838703f0b4eb1d4a49fbcfce870a79f3b6
wy0705 <2652918156@qq.com>
2021-11-23 21:40:24 +0800

    77

--- src/Solution4.java
+++ src/Solution4.java
@@ -38,3 +38,20 @@ class Solution6 {
 
     }
 }
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
+class Solution7 {
+    public ListNode deleteNode(ListNode head, int val) {
+        if(head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while(cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if(cur != null) pre.next = cur.next;
+        return head;
+    }
+}
